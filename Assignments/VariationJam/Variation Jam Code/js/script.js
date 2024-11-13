94a8229b7be093dae10aec5a21bdc50080186047
Yejin Oh <94939218+yejiweji@users.noreply.github.com>
2024-11-13 12:15:48 -0500

    Add Coding Folder
    nothing changed, uploading template

---
+++ Assignments/VariationJam/Variation Jam Code/js/script.js
@@ -0,0 +1,24 @@
+/**
+ * Title of Project
+ * Author Name
+ * 
+ * DESCRIPTION OF MY PROJECT!
+ * 
+ */
+
+"use strict";
+
+/**
+ * OH LOOK I DIDN'T DESCRIBE SETUP!!
+*/
+function setup() {
+
+}
+
+
+/**
+ * OOPS I DIDN'T DESCRIBE WHAT MY DRAW DOES!
+*/
+function draw() {
+
+}
